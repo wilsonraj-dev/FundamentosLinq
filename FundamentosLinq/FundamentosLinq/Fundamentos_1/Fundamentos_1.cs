df34c706fa1f8ea3619ac82764bac52ac92c269a
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-12 21:13:35 -0300

    Fundamentos 1
    Iniciando mostrando os fundamentos mais básicos com um exemplo usando a Query Syntax e Method Syntax

---
+++ FundamentosLinq/FundamentosLinq/Fundamentos_1/Fundamentos_1.cs
@@ -0,0 +1,23 @@
+﻿namespace FundamentosLinq
+{
+    class Fundamentos_1
+    {
+        static void Main(string[] args)
+        {
+            IList<string> frutas = new List<string>() { "Banana", "Maça", "Pera", "Laranja", "Uva" };
+
+            //Query syntax
+            var resultado = from f in frutas
+                            where f.Contains('r')
+                            select f;
+
+            Console.WriteLine(string.Join(" - ", resultado));
+
+            //Method syntax
+            var resultado2 = frutas.Where(f => f.Contains('r'));
+            Console.WriteLine(string.Join(" - ", resultado2));
+
+            Console.ReadKey();
+        }
+    }
+}

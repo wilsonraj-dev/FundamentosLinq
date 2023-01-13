b857bd1cdbe14ebaea2d9ba28e8970e2859019f6
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-12 21:24:19 -0300

    Fundamentos 4
    Primeira parte do tópico tratando sobre operadores de conjuntos. Nesse tópico de hoje foram mostrados os operados Distinct, DistinctBy, Except e ExceptBy

---
+++ FundamentosLinq/FundamentosLinq/Fundamentos_4/FonteDados.cs
@@ -0,0 +1,34 @@
+﻿namespace FundamentosLinq.Fundamentos_4
+{
+    public class FonteDados
+    {
+        public static int[] GetIdades()
+        {
+            var idades = new[] { 30, 33, 35, 36, 40, 30, 33, 36, 30, 40 };
+            return idades;
+        }
+
+        public static string[] GetNomes()
+        {
+            var nomes = new[] { "Paulo", "MARIA", "paulo", "Amanda", "maria", "amanda" };
+            return nomes;
+        }
+
+        public static List<Aluno> GetAlunos()
+        {
+            List<Aluno> alunos = new List<Aluno>()
+            {
+                new Aluno() { Nome = "Maria",  Idade = 42, },
+                new Aluno() { Nome = "Manoel", Idade = 34, },
+                new Aluno() { Nome = "Amanda", Idade = 21, },
+                new Aluno() { Nome = "Carlos", Idade = 18, },
+                new Aluno() { Nome = "Alicia", Idade = 15, },
+                new Aluno() { Nome = "Jaime",  Idade = 36, },
+                new Aluno() { Nome = "Debora",  Idade = 33, },
+                new Aluno() { Nome = "Sandra",  Idade = 19, },
+            };
+
+            return alunos;
+        }
+    }
+}
---
+++ FundamentosLinq/FundamentosLinq/Fundamentos_4/Fundamentos_4.cs
@@ -0,0 +1,65 @@
+﻿namespace FundamentosLinq.Fundamentos_4
+{
+    class Fundamentos_4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("## LINQ - Operações com conjuntos ## \n");
+
+            Console.WriteLine("Idades -> 30, 33, 35, 36, 40, 30, 33, 36, 30, 40");
+            var idadesDistintas = FonteDados.GetIdades().Distinct().ToList();
+            foreach (var idade in idadesDistintas)
+            {
+                Console.WriteLine($"{idade} ");
+            }
+
+
+            Console.WriteLine("Nomes -> Paulo, MARIA, paulo, Amanda, maria, amanda");
+            var nomesDistintos = FonteDados.GetNomes().Distinct(StringComparer.OrdinalIgnoreCase).ToList();
+            foreach (var nome in nomesDistintos)
+            {
+                Console.WriteLine($"{nome} ");
+            }
+
+
+            var alunos = FonteDados.GetAlunos().ToList();
+            var alunosIdadesDistintas = alunos.DistinctBy(a => a.Idade);
+            foreach (var aluno in alunosIdadesDistintas)
+            {
+                Console.WriteLine($"Aluno - {aluno.Nome} tem {aluno.Idade} anos");
+            }
+
+
+            List<int> fonte1 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> fonte2 = new List<int>() { 1, 3, 5, 8, 9, 10 };
+
+            var resultado = fonte1.Except(fonte2).ToList();
+            foreach (var item in resultado)
+            {
+                Console.WriteLine(item);
+            }
+
+            string[] paises1 = { "Brasil", "USA", "UK", "Argentina", "China" };
+            string[] paises2 = { "Brasil", "uk", "Argentina", "França", "Japão" };
+
+            var resultadoPaises = paises1.Except(paises2, StringComparer.OrdinalIgnoreCase).ToList();
+            foreach (var pais in resultadoPaises)
+            {
+                Console.WriteLine(pais);
+            }
+
+
+            var alunosExceptBy = FonteDados.GetAlunos();
+            var alunosReprovados = new List<string> { "Amanda", "Alicia", "Jaime" };
+            var alunosAprovados = alunosExceptBy.ExceptBy(alunosReprovados, n => n.Nome);
+
+            Console.WriteLine("Alunos Aprovados");
+            foreach (var aluno in alunosAprovados)
+            {
+                Console.WriteLine($"{aluno.Nome}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

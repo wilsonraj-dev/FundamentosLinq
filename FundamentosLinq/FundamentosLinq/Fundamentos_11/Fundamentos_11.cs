c9321315ead8a6ab79e96a390b2df97092c60318
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-03-16 22:00:58 -0300

    Fundamentos 11 - Parte 2 => Nesse exemplo foi feita a parte 2 dos operadores de junção, foram mostrados os operadores Right Join, Full Join, Cross Join e o Group Join

--- FundamentosLinq/FundamentosLinq/Fundamentos_11/Fundamentos_11.cs
+++ FundamentosLinq/FundamentosLinq/Fundamentos_11/Fundamentos_11.cs
@@ -10,7 +10,7 @@
             /// Decidi optar por não criar um novo projeto, pois para o objetivo desse 
             /// repositório mostrar como funciona o método já irá ajudar bastante.
             /// </summary>
-            
+
             #region Join
 
             #region innerJoin
@@ -58,7 +58,98 @@
             //                           NomeSetor = setor.Nome
             //                       }).ToList();
             #endregion
-            
+
+            #region rightJoin
+            ///<summary>
+            ///A LINQ não suporta um Right Join, por isso para contornar essa situação é possível
+            ///trocar as tabelas e fazer um Left Join. Assim teremos o comportamento esperado para
+            ///um Right Join
+            /// </summary>
+
+            //var rightJoin = (from s in contexto.Setores
+            //                 join f in contexto.Funcionarios
+            //                 on s.SetorId equals f.SetorId
+            //                 into SetorfunciGrupo
+            //                 from funcionario in SetorfunciGrupo.DefaultIfEmpty()
+            //                 select new
+            //                 {
+            //                     NomeFuncionario = funcionario.FuncionarioNome,
+            //                     CargoFuncionario = funcionario.FuncionarioCargo,
+            //                     NomeSetor = s.SetorNome
+            //                 }).ToList();
+            #endregion
+
+            #region fullJoin
+            ///<summary>
+            ///FullJoin é a união lógica entre um left join e um right join. Podemos usar o método
+            ///Union() para encontrar os elementos exclusivos entre duas sequências ou coleções
+            /// </summary>
+
+            //var leftJoin = (from f in contexto.Funcionarios
+            //                       join s in contexto.Setores
+            //                       on f.SetorId equals s.SetorId
+            //                       into funciSetorGrupo
+            //                       from setor in funciSetorGrupo.DefaultIfEmpty()
+            //                       select new
+            //                       {
+            //                           NomeFuncionario = f.FuncionarioNome,
+            //                           CargoFuncionario = f.FuncionarioCargo,
+            //                           NomeSetor = setor.Nome
+            //                       }).ToList();
+
+            //var rightJoin = (from s in contexto.Setores
+            //                 join f in contexto.Funcionarios
+            //                 on s.SetorId equals f.SetorId
+            //                 into SetorfunciGrupo
+            //                 from funcionario in SetorfunciGrupo.DefaultIfEmpty()
+            //                 select new
+            //                 {
+            //                     NomeFuncionario = funcionario.FuncionarioNome,
+            //                     CargoFuncionario = funcionario.FuncionarioCargo,
+            //                     NomeSetor = s.SetorNome
+            //                 }).ToList();
+
+            //var fullJoin = leftJoin.Union(rightJoin);
+            #endregion
+
+            #region crossJoin
+            ///<summary>
+            ///O crossJoin faz a junção de duas coleções para obter uma nova coleção onde
+            ///cada par combinado está representado. Essa associção não requer nenhuma condição na junção,
+            ///porém a LINQ não permite o uso da palavra-chave "join" sem nenhuma condição.
+            ///É preciso duas cláusulas "from", uma para cada coleção, para poder fazer uma junção cruzada,
+            ///e a seguir usar a cláusula "Select" para fazer uma projeção do resultado.
+            /// </summary>
+
+            //var crossJoin = from f in contexto.Funcionarios
+            //                from s in contexto.Setores
+            //                select new
+            //                {
+            //                    Nome = f.FuncionarioNome,
+            //                    Cargo = f.FuncionarioCargo,
+            //                    Setor = s.SetorNome
+            //                };
+            #endregion
+
+            #region GroupJoin
+            ///<summary>
+            ///O operador GroupJoin executa a mesma tarefa que o operador Join, porém ele irá
+            ///retornar um resultado em grupo com base na chave de grupo especificada. O GroupJoin
+            ///une duas coleções com base na chave e agrupa o resultado pela chave correspondente.
+            ///A seguir, retorna uma coleção de resultado e chave agrupados. O GroupJoin requer
+            ///os mesmos parâmetros que o Join.
+            /// </summary>
+
+            //var groupJoin = contexto.Setores
+            //                .GroupJoin(contexto.Funcionarios,
+            //                 s => s.SetorId, f => f.SetorId,
+            //                 (f, funcionariosGrupo) => new
+            //                 {
+            //                     Funcionarios = funcionariosGrupo,
+            //                     NomeSetor = f.SetorNome
+            //                 }).ToList();
+            #endregion
+
             #endregion
         }
     }

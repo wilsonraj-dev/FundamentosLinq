362697b92389d5e091d2da76613e4888864da8aa
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-03-30 20:38:58 -0300

    Fundamentos 14 - Mostrando como pode ser feita uma simples paginação utilizando os métodos Skip e Take

---
+++ FundamentosLinq/FundamentosLinq/Fundamentos_14/Fundamentos_14.cs
@@ -0,0 +1,36 @@
+﻿namespace FundamentosLinq.Fundamentos_14
+{
+    internal class Fundamentos_14
+    {
+        //Implementando a paginação
+        int RegistrosPorPagina = 4;
+        int NumeroPagina;
+
+        public void Metodo()
+        {
+            do
+            {
+                Console.Write("\nInforme o nº de página entre 1 e 4: ");
+                if (int.TryParse(Console.ReadLine(), out NumeroPagina))
+                {
+                    if (NumeroPagina > 0 && NumeroPagina < 5)
+                    {
+                        var alunos = Aluno.GetAlunos().Skip((NumeroPagina - 1) * RegistrosPorPagina)
+                                                      .Take(RegistrosPorPagina).ToList();
+
+                        Console.WriteLine("\nPágina. :" + NumeroPagina);
+
+                        foreach (var aluno in alunos)
+                        {
+                            Console.WriteLine($"Id: {aluno.Id} Nome: {aluno.Nome} Curso: {aluno.Curso}");
+                        }
+                    }
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            } while (true);
+        }
+    }
+}
